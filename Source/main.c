b47e1353f6a1b183d624446cd9cfd30c6cfc7283
ATmega8 <295017359@qq.com>
2016-03-21 11:39:00 +0000

    CRC测试

--- Source/main.c
+++ Source/main.c
@@ -1,35 +1,6 @@
 #include "main.h"
 
-uint32_t idleCount = 0UL;
-
-void vTask1(void *pvParameters )
-{
-	while(1)
-	{
-
-		printf("Task1\n");
-		vTaskDelay(250);
-	}
-}
-
-void vTask2(void *pvParameters )
-{
-	while(1)
-	{
-		printf("Task2\n");
-		vTaskDelay(250);
-	}
-}
-
-void vApplicationIdleHook(void)
-{
-	idleCount++;
-}
-
-void test_unity(void)
-{
-	TEST_ASSERT_EQUAL_INT(1, 2);	
-}
+#include "test_CRC.h"
 
 int main(void)
 {
@@ -38,30 +9,8 @@ int main(void)
 	USART_Config();
 #endif 
 	
-	UnityBegin("Uinty Test\n");
-	RUN_TEST(test_unity, 29);
+	UnityBegin("CRC Test");
+	RUN_CRCTest();
 	UnityEnd();
-
-
-	printf("Start\r\n");
-
-	xTaskCreate( vTask1, 
-			     "Task 1", 
-			     1000, 
-			     NULL, 
-			     1, 
-			     NULL);
-
-
-	xTaskCreate( vTask2, 
-			     "Task 2", 
-			     1000, 
-			     NULL, 
-			     1, 
-			     NULL);
-
-	//vTaskStartScheduler();
-			
-
 }
 
