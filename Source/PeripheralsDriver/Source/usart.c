52ba5b1106ed9fd8fac46d8d69412cd6406f0cb1
ATmega8 <liuyang.2014@foxmail.com>
2016-04-06 16:23:52 +0000

    将MODBUS  USART 相关参数替换为宏定义

--- Source/PeripheralsDriver/Source/usart.c
+++ Source/PeripheralsDriver/Source/usart.c
@@ -1,4 +1,5 @@
 #include "stm32f4xx_conf.h"
+#include "modbus_define.h"
 
 /*2000000 8N1 */
 void USART_Config(void)
@@ -8,59 +9,48 @@ void USART_Config(void)
     NVIC_InitTypeDef NVIC_InitStructure;
 
     /*时钟设置*/
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(MODBUS_USART_CLOCK, ENABLE);
+    RCC_AHB1PeriphClockCmd(MODBUS_USART_GPIO_CLOCK, ENABLE);
 	
     /*GPIO设置*/
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
 
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+	GPIO_PinAFConfig(MODBUS_USART_RX_GPIO_PORT, 
+			MODBUS_USART_RX_GPIO_PINSOURCE, MODBUS_USART_GPIO_AF);
 
-    GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_AF;
-    GPIO_InitStructure.GPIO_OType   = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd    = GPIO_PuPd_NOPULL;
-    GPIO_InitStructure.GPIO_Speed   = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_3;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_PinAFConfig(MODBUS_USART_TX_GPIO_PORT, 
+			MODBUS_USART_TX_GPIO_PINSOURCE, MODBUS_USART_GPIO_AF);
 
 	/*TX*/
-    GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Pin     = MODBUS_USART_TX_GPIO_PIN;
     GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_OType   = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_PuPd    = GPIO_PuPd_NOPULL;
     GPIO_InitStructure.GPIO_Speed   = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_Init(MODBUS_USART_TX_GPIO_PORT, &GPIO_InitStructure);
 
 	/*RX*/
-    GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_7;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin     = MODBUS_USART_RX_GPIO_PIN;
+    GPIO_Init(MODBUS_USART_RX_GPIO_PORT, &GPIO_InitStructure);
 
 	/*DE*/
-    GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Pin     = MODBUS_DE_GPIO_PIN;
 	GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_OUT;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_Init(MODBUS_DE_GPIO_PORT, &GPIO_InitStructure);
 
     /*USART通用设置*/
-    USART_InitStructure.USART_BaudRate   = 115200;
+    USART_InitStructure.USART_BaudRate   = MODBUS_USART_BAUD_RATE;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits   = USART_StopBits_1;
     USART_InitStructure.USART_Parity    = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode       = USART_Mode_Tx | USART_Mode_Rx;
-    USART_Init(USART1, &USART_InitStructure);
+    USART_Init(MODBUS_USART, &USART_InitStructure);
 
     USART_InitStructure.USART_Mode       = USART_Mode_Tx | USART_Mode_Rx;
-    USART_Init(USART1, &USART_InitStructure);
+    USART_Init(MODBUS_USART, &USART_InitStructure);
 
 	/*串口接收中断*/
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	USART_ITConfig(MODBUS_USART, USART_IT_RXNE, ENABLE);
 
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;
@@ -69,6 +59,6 @@ void USART_Config(void)
 	NVIC_Init(&NVIC_InitStructure);
 
     /*使能USART*/
-    USART_Cmd(USART1, ENABLE);
+    USART_Cmd(MODBUS_USART, ENABLE);
 }
 
