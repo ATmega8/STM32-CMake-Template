a0e231501121d5c29ef379895e1c56bb95ce5b85
ATmega8 <liuyang.2014@foxmail.com>
2016-04-08 10:08:49 +0000

    加入MODBUS解析

--- Source/main.c
+++ Source/main.c
@@ -20,31 +20,30 @@ int main(void)
 {
 	MOTOR_Init();
 	MOTOR_Start();
-	MOTOR_Forward();
-	//NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
-	//USART_Config();
+
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+	USART_Config();
 
 	/*初始化信号量*/
-	//frameLengthQueue = xQueueCreate(7, sizeof(uint32_t));
-	//errorQueue = xQueueCreate(7, sizeof(MODBUSTypeDef*));
+	frameLengthQueue = xQueueCreate(7, sizeof(uint32_t));
+	errorQueue = xQueueCreate(7, sizeof(MODBUSTypeDef*));
 
 
 	/*初始化环形缓冲区*/
-	//ptxcbuf = CircularBuffer_Create(56, sizeof(uint32_t));
-	//prxcbuf = CircularBuffer_Create(56, sizeof(uint32_t));
+	ptxcbuf = CircularBuffer_Create(56, sizeof(uint32_t));
+	prxcbuf = CircularBuffer_Create(56, sizeof(uint32_t));
 
 	/*更新 MODBUS 寄存器*/
-	//MODBUS_RegisterUpdate();
+	MODBUS_RegisterUpdate();
 
-	//if(frameLengthQueue == NULL)
-		//printf("%s %d: Queue Create Fault!", __FUNCTION__, __LINE__ );
+	if(frameLengthQueue == NULL)
+		printf("%s %d: Queue Create Fault!", __FUNCTION__, __LINE__ );
 
 	/*任务栈大小必须大于 configMINIMAL_STACK_SIZE !!!! */
-	//xTaskCreate(MODBUS_CheckRequestTask, "TASK2", 1024, NULL,  1, NULL);
-	//xTaskCreate(MODBUS_ReplyTask, "TASK1", 1024, NULL,  1, NULL);
+	xTaskCreate(MODBUS_CheckRequestTask, "TASK2", 1024, NULL,  1, NULL);
 
 	/*开始调度*/
-	//vTaskStartScheduler();
+	vTaskStartScheduler();
 	
 	while(1)
 	{
